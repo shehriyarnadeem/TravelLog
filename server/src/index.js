cc46ebd6e7434ec50d93f7a5c4d1c5d8035888ed
Shehriyar Nadeem <shehriyarnadeem@Shehriyars-MacBook-Pro.local>
2020-02-29 23:11:23 +0500

    first commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,37 +1,41 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const logs = require('./api/logs');
+const middlewares = require("./middlewares");
+const logs = require("./api/logs");
 
 const app = express();
 
-app.enable('trust proxy'); // needed for rate limiting by Client IP
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-app.use(morgan('common'));
+app.use(morgan("common"));
 app.use(helmet());
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+  })
+);
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Hello World!',
+    message: "Hello World!"
   });
 });
 
-app.use('/api/logs', logs);
+app.use("/api/logs", logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
