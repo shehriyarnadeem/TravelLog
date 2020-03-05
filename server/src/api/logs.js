9a471579c9dcee1775784c0ec975ef4c63446408
Shehriyar Nadeem <shehriyarnadeem@Shehriyars-MacBook-Pro.local>
2020-03-05 13:52:24 +0500

    Push

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 const LogEntry = require("../models/LogEntry");
-
+const { API_KEY } = process.env;
 const router = Router();
 
 router.get("/", async (req, res, next) => {
@@ -15,6 +15,9 @@ router.get("/", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
+    // if (req.get("X-API-KEY") !== API_KEY) {
+    //   throw new Error("UnAuthorized");
+    // }
     const logEntry = new LogEntry(req.body);
     const createdEntry = await logEntry.save();
     res.json(createdEntry);
